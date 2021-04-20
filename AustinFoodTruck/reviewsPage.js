6f1d47fb579b0334635dfc99b5549e559af06dab
EdoardoPalazzi <edoardo.palazzi@gmail.com>
2021-04-19 20:04:21 -0500

    reviews page updates

--- AustinFoodTruck/reviewsPage.js
+++ AustinFoodTruck/reviewsPage.js
@@ -1,11 +1,65 @@
+selections = ["all", "best", "italian", "sa", "mexican", "asian", "mediterranean"]
+
 function selection(value){
    if (value == "all"){
       document.getElementById("all").style.display = "block";
-      document.getElementById("best").style.display = "none";
+      for (let i = 0; i <= selections.length; i++){
+         if (selections[i] != value){
+            document.getElementById(selections[i]).style.display = "none";
+         }
+      }
    }
 
    if (value == "best"){
-      document.getElementById("all").style.display = "none";
       document.getElementById("best").style.display = "block";
+      for (let i = 0; i <= selections.length; i++){
+         if (selections[i] != value){
+            document.getElementById(selections[i]).style.display = "none";
+         }
+      }
+   }
+
+   if (value == "italian"){
+      document.getElementById("italian").style.display = "block";
+      for (let i = 0; i <= selections.length; i++){
+         if (selections[i] != value){
+            document.getElementById(selections[i]).style.display = "none";
+         }
+      }
+   }
+
+   if (value == "sa"){
+      document.getElementById("sa").style.display = "block";
+      for (let i = 0; i <= selections.length; i++){
+         if (selections[i] != value){
+            document.getElementById(selections[i]).style.display = "none";
+         }
+      }
+   }
+   if (value == "mexican"){
+      document.getElementById("mexican").style.display = "block";
+      for (let i = 0; i <= selections.length; i++){
+         if (selections[i] != value){
+            document.getElementById(selections[i]).style.display = "none";
+         }
+      }
+   }
+
+   if (value == "asian"){
+      document.getElementById("asian").style.display = "block";
+      for (let i = 0; i <= selections.length; i++){
+         if (selections[i] != value){
+            document.getElementById(selections[i]).style.display = "none";
+         }
+      }
+   }
+
+   if (value == "mediterranean"){
+      document.getElementById("mediterranean").style.display = "block";
+      for (let i = 0; i <= selections.length; i++){
+         if (selections[i] != value){
+            document.getElementById(selections[i]).style.display = "none";
+         }
+      }
    }
 }
